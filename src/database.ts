f2328cb5fdb23b467b0ada2ff9b916ce469ca48c
thiagomayrink <thiagoscmpaiva@gmail.com>
2021-07-23 20:37:49 -0300

    setup for heroku deploy

--- src/database.ts
+++ src/database.ts
@@ -2,12 +2,13 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const connection = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  database: process.env.DB_DATABASE,
-});
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
+}
 
-export {connection};
+const connection = new Pool(databaseConfig);
+
+export { connection };
